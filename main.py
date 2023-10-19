838693ff00171fe348e1b0e2449e82fe88f34b4b
PogrebniakIllia <142676283+PogrebniakIllia@users.noreply.github.com>
2023-10-19 21:09:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from ui import Ui_MainWindow
+import random
 
 class Widget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.btn_OK.clicked.connect(self.generate)
 
-def generation():
-    digital =['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    letter = ['g', 'r', 's', 'k', 'm', 'a', 'i', 'x', 'p', 'b']
+    def generate(self):
+        s_number = '0123456789'
+        s_alfabet = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+        pas = ''
+        if self.ui.cl_number.isChecked():
+            for i in range(16):
+                pas = pas + random.choice(s_number)
+        if self.ui.cl_alphabet.isChecked():
+            for i in range(16):
+                pas = pas + random.choice(s_alfabet)
+        if self.ui.cl_number.isChecked() and self.ui.cl_alphabet.isChecked():
+            for i in range(16):
+                pas = pas + random.choice(s_number + s_alfabet)
+        self.ui.result.setText(pas)
+        
+
 
 app = QApplication([])
 window = Widget()
 
-btn_OK.clicked.connect(generation)
 
 window.show()
 app.exec_()
